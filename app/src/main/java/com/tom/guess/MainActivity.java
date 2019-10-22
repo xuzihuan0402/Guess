ceaced210e6746fa0ae912f7d0f9240f34820d88
miia <miia@com.tw>
2019-10-22 16:58:21 +0800

    2

--- app/src/main/java/com/tom/guess/MainActivity.java
+++ app/src/main/java/com/tom/guess/MainActivity.java
@@ -8,14 +8,28 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView message;
+    int secret = new Random().nextInt(10)+1;
+    String TAG = MainActivity.class.getSimpleName();
+    private EditText edNumber;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG,"secret"+secret);
+        edNumber = findViewById(R.id.ConstraintLayout);
+        message = findViewById(R.id.message);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -32,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+
+
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -51,5 +67,17 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void guess(View view){
+        int number = Integer.parseInt(edNumber.getText().toString());
+        if(number<secret){
+            message.setText("Bigger");
+        }else if(number>secret){
+            message.setText("Small");
+        }else{
+            message.setText("Bingo!");
+        }
+
     }
 }
